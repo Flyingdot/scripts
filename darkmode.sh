692e1f3ae5ee13be7cd525f75c60b58fa7e3cb2a
Dani Meier <me@danimeier.dev>
2024-03-12 22:31:57 +0100

    feat(darkmode): switch vim colortheme

--- darkmode.sh
+++ darkmode.sh
@@ -4,7 +4,9 @@ if [ "$DARKMODE" -eq "0" ]
 then
     echo "Switch to light"
     kitty +kitten themes --reload-in=all "Catppuccin Kitty Latte"
+    echo "vim.cmd.colorscheme('catppuccin-latte')" > ~/.config/nvim/after/plugin/catppuccin.lua
 else
     echo "Switch to dark"
     kitty +kitten themes --reload-in=all "Catppuccin Kitty Mocha"
+    echo "vim.cmd.colorscheme('catppuccin-mocha')" > ~/.config/nvim/after/plugin/catppuccin.lua
 fi
